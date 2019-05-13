5ad6b476bf9ed4d319e7d8e6ca0c69462b2aefa1
mrtplk <martapalka13@gmail.com>
2019-05-14 01:05:22 +0200

    Initial Commit

---
+++ symulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  symulation
+//
+//  Created by Marta Pałka on 14.05.2019.
+//  Copyright © 2019 Marta Pałka. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
